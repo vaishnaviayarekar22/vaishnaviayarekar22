73b6d52ca3ebb054a577460f1303f09ea12c7099
Vaishnavi Ananda Ayarekar <vaishnaviayarekar22@gmail.com>
2025-08-07 10:35:33 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{ 	int age;
+ 	cout<<"Enter the age :";
+ 	cin>>age;
+ 	if(age>=18)
+ 	    {
+ 		cout<<"You are eligible for voting";
+ 	    }
+ 	else
+ 	    {
+ 		cout<<"You are not eligible";
+ 	    }
+ 	return 0;    
+ }
