a08f291dee9c9b7044c0574609d9175c471ddcf6
Vaishnavi Ananda Ayarekar <vaishnaviayarekar22@gmail.com>
2026-01-21 12:28:07 +0530

    Add files via upload

---
+++ Compair.java
@@ -0,0 +1,17 @@
+int a=10;
+int b=20;
+public class Compaire
+{
+public static void main( String[] args)
+{
+if(a>b)
+{
+System.out.println("A is Greter than  B ");
+}
+else
+{
+System.out.println("B is greter than A"); 
+}
+}
+}
+
